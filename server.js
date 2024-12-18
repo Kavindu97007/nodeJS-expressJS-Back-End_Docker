7b66f82ec5f63a6ef508b6589c6ce6e59b9bd13f
Kavindu97007 <gkpathirana7@gmail.com>
2024-12-18 21:01:22 +0530

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.send('Hello! Welcome to the Express Server with Docker...');
+    res.send('Hello! Welcome to the Express Server with Docker..');
 });
 
 // Define another route for demonstration
